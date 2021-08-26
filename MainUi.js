ad442f1821961f141916bb71ec7d1823355ee461
RobotEngineSchool <89589945+RobotEngineSchool@users.noreply.github.com>
2021-08-26 09:17:27 -0500

    Update MainUi.js

--- MainUi.js
+++ MainUi.js
@@ -6,6 +6,7 @@ createElement("CoreApp", "div", "body", {
   "width": "800px",
   "max-width": "100%",
   "top": "0px",
+  "height": "100vh",
   "left": "50%",
   "transform": "translateX(-50%)",
   "background-color": StyleColors.PageColor
