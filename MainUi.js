25ce570164d8f8ed28772d683bd2ea6f41820848
RobotEngineSchool <89589945+RobotEngineSchool@users.noreply.github.com>
2021-08-26 09:16:28 -0500

    Create MainUi.js

---
+++ MainUi.js
@@ -0,0 +1,13 @@
+var StyleColors = {
+  PageColor = "#E5E5E5",
+};
+
+createElement("CoreApp", "div", "body", {
+  "width": "800px",
+  "max-width": "100%",
+  "top": "0px",
+  "left": "50%",
+  "transform": "translateX(-50%)",
+  "background-color": StyleColors.PageColor
+});
+
