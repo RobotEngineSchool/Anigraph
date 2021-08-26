2110fa6cc9f050224e298f502c4a8d69860e6cad
RobotEngineSchool <89589945+RobotEngineSchool@users.noreply.github.com>
2021-08-26 10:24:28 -0500

    Update MainUi.js

--- MainUi.js
+++ MainUi.js
@@ -1,15 +1,24 @@
 var StyleColors = {
-  PageColor: "#E5E5E5",
+  Page: "#E5E5E5",
+  NavBar: "#FFFFFF"
 };
 
-setCSS("body", "background-color", StyleColors.PageColor);
+setCSS("body", "background-color", StyleColors.Page);
 
-createElement("CoreApp", "div", "body", {
+var CoreApp = createElement("CoreApp", "div", "body", {
   "width": "800px",
   "max-width": "100%",
-  "top": "0px",
-  "height": "100vh",
   "left": "50%",
+  "top": "0px",
   "transform": "translateX(-50%)"
+});
+
+createElement("NavBar", "div", CoreApp, {
+  "position": "sticky",
+  "width": "calc(100% - 16px)",
+  "height": "150px",
+  "left": "8px",
+  "top": "8px",
+  "background-color": StyleColors.NavBar
 });
 
